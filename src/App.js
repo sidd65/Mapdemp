9d4415f748bbcc6a62ec501e2a4c63782994cd09
Sid vaidya <sidvaidya65@gmail.com>
2020-03-29 08:12:37 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import dummyData from '../src/tempData/data';
+import RouteComponet from './componets/routeComponet'
 function App() {
+  const [viewport, setViewport] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    latitude: 45.4211,
+    longitude: -75.6903,
+    zoom: 10
+  });
+  const [selectedPark, setSelectedPark] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMapGL
+        {...viewport}
+        // mapStyle="mapbox://styles/siddharthvaidya/ck8d3snzz2gy71inwoouscjzm"
+        mapboxApiAccessToken="pk.eyJ1Ijoic2lkZGhhcnRodmFpZHlhIiwiYSI6ImNrOGQzY3E5djByb3AzZW1qZjVocTE1NDgifQ.fT4SdJA-bKajoF4Gv_0B6w"
+        onViewportChange={setViewport}
+      >
+        <RouteComponet points={dummyData} />
+        {
+          dummyData.map((s)=>
+          (
+            <Marker key={s.id}
+            latitude={ s.geometry.coordinates[1] }
+            longitude={ s.geometry.coordinates[0] }
+            >
+              <button 
+              onClick={(e)=>{
+                e.preventDefault();
+                setSelectedPark(s);
+              }}
+              className="marker-btn">
+                <img src={require('./assets/images/marker.png')} alt="marker icon"></img>
+              </button>
+            </Marker>
+          ))
+        }
+        {selectedPark ? (
+          <Popup
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedPark(null);
+            }}
+          >
+            <div>
+              <p>{selectedPark.description}</p>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
     </div>
   );
 }
---
+++ src/tempData/data.js
@@ -0,0 +1,106 @@
+const dummyData = [
+    {
+        "id": 1,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.3372987731628,
+                45.383321536272049
+            ]
+        },
+        "description": "Centre communautaire de Fitzroy Harbour - Lieu de planchodrome itinérant"
+    },
+    {
+        "id": 2,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.546518086577947,
+                45.467134581917357
+            ]
+        },
+        "description": "eighbourhood : smaller size facility to service population of 10,000 or less"
+
+    },
+    {
+        "id": 3,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.898610599532319,
+                45.295014379864874
+            ]
+        },
+        "description": "e voisinage : petite installation assurant des services à 10 000 résidents ou moins."
+
+    },
+    {
+        "id": 4,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.468561642270757,
+                45.23032561834377
+            ]
+        },
+        "description": "Grand bol de béton, modules de rue et modules verticaux, 17 000 pi2"
+    },
+    {
+        "id": 5,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.926651366520872,
+                45.260659774950561
+            ]
+        },
+        "description": "Johnny Leroux Stittsville Community Arena - Roving Skateboard Park Location"
+
+    },
+    {
+        "id": 6,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.760933332842754,
+                45.345566668964558
+            ]
+        },
+        "description": "Grand bol de béton, modules de rue et modules verticaux, 17 000 pi2"
+    },
+    {
+        "id": 7,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.625996131485707,
+                45.375401587496128
+            ]
+        },
+        "description": "e voisinage : petite installation assurant des services à 10 000 résidents ou moins."
+    },
+    {
+        "id": 8,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -75.857009812435976,
+                45.290758029776626
+            ]
+        },
+        "description": "Johnny Leroux Stittsville Community Arena - Roving Skateboard Park Location"
+    },
+    {
+        "id": 9,
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -76.206087708136721,
+                45.470459866077654
+            ]
+        },
+        "description": "Johnny Leroux Stittsville Community Arena - Roving Skateboard Park Location"
+    }
+]
+
+export default dummyData;
